b52ebee98fb961b25de0be73558eaffaa7a30fe4
Alphonse Xavier <alphonsemarslinarulxavier@gmail.com>
2025-02-16 19:58:47 -0500

    Added navbar and others

--- client/src/App.js
+++ client/src/App.js
@@ -4,40 +4,27 @@ import SignupRecruiter from "./signup/SignupRecruiter";
 import LoginStudent from "./login/LoginStudent";
 import LoginRecruiter from "./login/LoginRecruiter";
 import StudentProfile from "./profile/StudentProfile";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import CompanyProfile from "./profile/CompanyProfile";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./landing/Landing";
+import Navbar from "./navigation/Navbar";
 
 function App() {
-  const route = createBrowserRouter([
-    {
-      path: "/",
-      element: <Landing />,
-    },
-    {
-      path: "/signup/professional",
-      element: <SignupStudent />,
-    },
-    {
-      path: "/signup/recruiter",
-      element: <SignupRecruiter />,
-    },
-    {
-      path: "/login/professional",
-      element: <LoginStudent />,
-    },
-    {
-      path: "/login/recruiter",
-      element: <LoginRecruiter />,
-    },
-    {
-      path: "/professional/profile",
-      element: <StudentProfile />,
-    },
-  ]);
   return (
-    <div className="App">
-      <RouterProvider router={route}></RouterProvider>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signup/professional" element={<SignupStudent />} />
+          <Route path="/signup/recruiter" element={<SignupRecruiter />} />
+          <Route path="/login/professional" element={<LoginStudent />} />
+          <Route path="/login/recruiter" element={<LoginRecruiter />} />
+          <Route path="/professional/profile" element={<StudentProfile />} />
+          <Route path="/recruiter/profile" element={<CompanyProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
