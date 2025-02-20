bd197152fe5678896a80b66b396e6d84efa1056b
Nayana Kamerkar <nayana.k@expertrons.in>
2025-02-19 19:21:44 -0500

    backend setup

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+
+// Configuring the database
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
