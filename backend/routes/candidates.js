2cf3bcfd19b2a63e9f16bd2c2e141b1e7dc6f886
Alphonse Xavier <alphonsemarslinarulxavier@gmail.com>
2025-03-03 08:30:38 -0500

    Update candidates.js with login and signup functionality

--- backend/routes/candidates.js
+++ backend/routes/candidates.js
@@ -1,23 +1,29 @@
-require('module-alias/register')
-const express = require('express')
+require("module-alias/register");
+const express = require("express");
 const router = express.Router();
 const candidateController = require("@controllers/candidates");
 const Validation = require("@validation");
-const Responder =  require("@service/responder");
-// const userMiddleware = require('../middleware/userMiddleware');
+const Responder = require("@service/responder");
 
-router.post('/signup', 
-  Validation.signup(), 
+router.post(
+  "/signup",
+  Validation.signup(),
   Responder.validate.bind(Responder),
   candidateController.signup.bind(candidateController)
 );
 
-router.post('/editProfile',
+router.post(
+  "/editProfile",
   Validation.editProfile(),
   Responder.validate.bind(Responder),
   candidateController.editProfile.bind(candidateController)
-)
+);
 
-
+router.post(
+  "/login",
+  Validation.login(),
+  Responder.validate.bind(Responder),
+  candidateController.login.bind(candidateController)
+);
 
 module.exports = router;
